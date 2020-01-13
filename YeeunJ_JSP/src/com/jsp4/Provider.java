d6a9838acd978661136ae0c499d68584862b3f97
jeong-yeeun <jeong-yeeun@172.17.196.71>
2020-01-13 15:31:40 +0900

    jsp4 전

---
+++ YeeunJ_JSP/src/com/jsp4/Provider.java
@@ -0,0 +1,8 @@
+package com.jsp4;
+
+public class Provider {
+	static String DRIVER="com.mysql.cj.jdbc.Driver";  
+	static String CONNECTION_URL="jdbc:mysql://db4free.net:3306/yeeunj?useSSL=false&serverTimezone=UTC&characterEncoding=utf8";  
+	static String USERNAME="yeeunj";  
+	static String PASSWORD="dPdms3730";  
+}
